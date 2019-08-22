e721f8fbddad63bc5eff72e0640643f5bd66331e
George <george.abraham.ro@gmail.com>
2019-08-22 15:40:47 +0100

    message

---
+++ arch.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# a comment
+
+# will check if any arguments were passed to the program
+if [ $# -lt 3 ]
+    then
+	echo "No arguments supplied\nProvide 3 arguments" 
+#    else
+#	echo "$#"
+fi
+
+if [ $# -ge 3 ]
+then
+    echo "Script has at least 3 arguments:\n$1, $2, $3"
+fi

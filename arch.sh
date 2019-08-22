362a5cafa76f6b063da57c4a090d7d5b06630ae4
georgeabr <george.abraham.ro@gmail.com>
2019-08-22 16:09:33 +0100

    added

--- arch.sh
+++ arch.sh
@@ -13,14 +13,19 @@
 # will check if any arguments were passed to the program
 if [ $# -lt 3 ]
     then
-	echo "No arguments supplied\nProvide 3 arguments" 
+	echo "No arguments supplied. Provide 3 arguments:\n1. UEFI drive\n2. root drive\n3. swap drive" 
 #    else
 #	echo "$#"
 fi
 
+uefi_boot="/dev/$1"
+root_drive="/dev/$2"
+swap_drive="/dev/$3"
+
 if [ $# -ge 3 ]
 then
-    echo "Script has at least 3 arguments:\n$1, $2, $3"
+	# echo "Script has at least 3 arguments:\n$1, $2, $3"
+	echo "Will use\n$uefi_boot for UEFI\n$root_drive for root\n$swap_drive for swap"
 fi
 
 # test

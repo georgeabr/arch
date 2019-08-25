b72dc31323c2f596ca52b86a0103e06c6bb91420
georgeabr <george.abraham.ro@gmail.com>
2019-08-25 15:49:39 +0100

    added

--- arch.sh
+++ arch.sh
@@ -29,51 +29,52 @@
 # will check if any arguments were passed to the program
 if [ $# -lt 3 ]
     then
-	printf "No arguments supplied. Provide 3 arguments:\n1. UEFI drive\n2. root drive\n3. swap drive\n" 
+	printf "No arguments supplied. Provide 3 arguments:\n1. UEFI drive\n2. root drive\n3. swap drive\n";
+	fdisk -l
 exit 0
 #    else
 #	echo "$#"
 fi
 
-uefi_boot="/dev/$1"
+uefi_drive="/dev/$1"
 root_drive="/dev/$2"
 swap_drive="/dev/$3"
 
 if [ $# -ge 3 ]
 then
 	# echo "Script has at least 3 arguments:\n$1, $2, $3"
-	printf "Will use\n$uefi_boot for UEFI\n$root_drive for root\n$swap_drive for swap\n"
+	printf "Will use\n$uefi_drive for UEFI\n$root_drive for root\n$swap_drive for swap\n"
 fi
 
 
 go_ahead()
 {
-	printf "\nPart 1 - Initial disk formatting/bootstrap/installation.\n";
-	printf "Creating new GPT table\n";
-	parted -s /dev/sda mklabel gpt
+	printf "\nPart 1 - Initial Arch bootstrap/installation.\n";
+	# printf "Creating new GPT table\n";
+	# parted -s /dev/sda mklabel gpt
 	
-	printf "Creating UEFI partition - 128M.\n"
-	parted -s /dev/sda mkpart primary FAT32 1 128MiB
-	parted -s /dev/sda set 1 esp on
-	printf "Formatting UEFI partition.\n"
-	mkfs.fat -F32 /dev/sda1
+	# printf "Creating UEFI partition - 128M.\n"
+	# parted -s /dev/sda mkpart primary FAT32 1 128MiB
+	# parted -s /dev/sda set 1 esp on
+	# printf "Formatting UEFI partition.\n"
+	# mkfs.fat -F32 /dev/sda1
 
-	printf "Creating SWAP partition - 1GB.\n";
-	parted -s /dev/sda mkpart primary linux-swap 128MiB 1129MiB
-	printf "Formatting SWAP partition.\n"	
-	mkswap /dev/sda2
+	# printf "Creating SWAP partition - 1GB.\n";
+	# parted -s /dev/sda mkpart primary linux-swap 128MiB 1129MiB
+	# printf "Formatting SWAP partition.\n"	
+	# mkswap /dev/sda2
 	printf "Activating SWAP partition.\n"
-	swapon /dev/sda2
+	swapon $swap_drive
 	
-	printf "Creating ROOT partition - rest of the disk.\n";
-	parted -s /dev/sda mkpart primary ext4 1129MiB 100%
-	printf "Formatting ROOT parition as ext4.\n"
-	mkfs.ext4 /dev/sda3
+	# printf "Creating ROOT partition - rest of the disk.\n";
+	# parted -s /dev/sda mkpart primary ext4 1129MiB 100%
+	# printf "Formatting ROOT parition as ext4.\n"
+	# mkfs.ext4 /dev/sda3
 
 	printf "Mounting UEFI, ROOT partitions.\n"
-	mount /dev/sda3 /mnt
+	mount $root_drive /mnt
 	mkdir -p /mnt/boot/EFI
-	mount /dev/sda1 /mnt/boot/EFI
+	mount $uefi_drive /mnt/boot/EFI
 
 	printf "Setting systemd NTP clock sync.\n"
 	timedatectl set-ntp true

662b9b1099df6956bdeeb37b97b1efe53229dd1c
George <george@fedora.home>
2019-12-25 18:33:52 +0000

    modified

---
+++ VM-zfs-root/arch-2.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# curl https://raw.githubusercontent.com/georgeabr/arch/master/arch-2.sh > arch-2.sh; chmod +x arch-2.sh; cp ./arch-2.sh /mnt; arch-chroot /mnt /bin/bash -c "./arch-2.sh"
+
+
+printf "\n\nPart 2 - continuing install/customisation.\nConfiguring locale to LONDON/UK.\n"
+rm -rf /etc/localtime
+ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
+hwclock --systohc --utc
+grep -rl "#en_GB.UTF-8 UTF-8" /etc/locale.gen | xargs sed -i 's/#en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/g'
+echo LANG=en_GB.UTF-8 > /etc/locale.conf
+export LANG=en_GB.UTF-8
+echo "KEYMAP=uk" > /etc/vconsole.conf
+locale-gen
+
+printf "Configuring hostname\n."
+echo archie > /etc/hostname
+	
+printf "Enabling DHCP.\n"
+systemctl enable dhcpcd.service
+
+printf "Enabling SSH.\n"
+pacman -Sy --noconfirm openssh
+systemctl enable sshd.service
+
+printf "Enter ROOT user password:\n"
+passwd root
+prinf "Adding user _george_, sudo permission\n"
+useradd -m -G wheel -s /bin/bash george
+grep -rl "# %wheel ALL=(ALL) ALL" /etc/sudoers | xargs sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g'
+printf "Enter password for user _george_\n"
+passwd george
+
+# %wheel ALL=(ALL) ALL
+# grep -rl "# %wheel ALL=(ALL) ALL" /etc/sudoers | xargs sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g'
+
+printf "Installing GRUB.\n"
+pacman -Sy --noconfirm grub efibootmgr dosfstools os-prober mtools
+grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --efi-directory=/boot/EFI/ --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+mkinitcpio -p linux
+
+printf "Installing Xorg, XFCE, fonts.\n"
+pacman -Sy --noconfirm xorg xterm xorg-drivers mc
+pacman -Sy --noconfirm xfce4 sddm mousepad ttf-dejavu ttf-bitstream-vera ttf-liberation noto-fonts
+pacman -Sy --noconfirm git networkmanager networkmanager-openvpn nm-connection-editor network-manager-applet wget curl firefox
+systemctl enable sddm.service
+systemctl enable NetworkManager
+timedatectl set-ntp true
+
+printf "\n"
+read -p "Work done. Press enter to exit and reboot."
+exit
+umount -a
+reboot
---
+++ VM-zfs-root/arch.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+# git clone https://github.com/georgeabr/arch.git
+# git config --global user.email "email@gmail.com"
+# git config --global user.name "georgeabr"
+
+# to save passwords
+# git config credential.helper store
+
+# commit the code
+# git add .; git commit -m "added"; git push -u origin master
+
+# to download script from git
+# https://raw.githubusercontent.com/georgeabr/arch/master/arch.sh
+# or
+# https://raw.githubusercontent.com/georgeabr/arch/master/arch.sh
+# wget https://bit.ly/2ZoJvnW -O arch.sh
+
+# to enable ssh connection to livecd install
+# set password for root user
+# passwd
+# systemctl start sshd.service
+
+# rm *; curl https://raw.githubusercontent.com/georgeabr/arch/master/arch.sh > arch.sh; chmod +x arch.sh
+
+# parted examples
+# https://wiki.archlinux.org/index.php/Parted#UEFI/GPT_examples
+
+# will check if any arguments were passed to the program
+if [ $# -lt 3 ]
+    then
+	printf "No arguments supplied. Provide 3 arguments:\n1. UEFI drive\n2. root drive\n3. swap drive\n" 
+exit 0
+#    else
+#	echo "$#"
+fi
+
+uefi_boot="/dev/$1"
+root_drive="/dev/$2"
+swap_drive="/dev/$3"
+
+if [ $# -ge 3 ]
+then
+	# echo "Script has at least 3 arguments:\n$1, $2, $3"
+	printf "Will use\n$uefi_boot for UEFI\n$root_drive for root\n$swap_drive for swap\n"
+fi
+
+
+go_ahead()
+{
+	printf "\nPart 1 - Initial disk formatting/bootstrap/installation.\n";
+	printf "Creating new GPT table\n";
+	parted -s /dev/sda mklabel gpt
+	
+	printf "Creating UEFI partition - 128M.\n"
+	parted -s /dev/sda mkpart primary FAT32 1 128MiB
+	parted -s /dev/sda set 1 esp on
+	printf "Formatting UEFI partition.\n"
+	mkfs.fat -F32 /dev/sda1
+
+	printf "Creating SWAP partition - 1GB.\n";
+	parted -s /dev/sda mkpart primary linux-swap 128MiB 1129MiB
+	printf "Formatting SWAP partition.\n"	
+	mkswap /dev/sda2
+	printf "Activating SWAP partition.\n"
+	swapon /dev/sda2
+	
+	printf "Creating ROOT partition - rest of the disk.\n";
+	parted -s /dev/sda mkpart primary ext4 1129MiB 100%
+	printf "Formatting ROOT parition as ext4.\n"
+	mkfs.ext4 /dev/sda3
+
+	printf "Mounting UEFI, ROOT partitions.\n"
+	mount /dev/sda3 /mnt
+	mkdir -p /mnt/boot/EFI
+	mount /dev/sda1 /mnt/boot/EFI
+
+	printf "Setting systemd NTP clock sync.\n"
+	timedatectl set-ntp true
+
+	printf "Updating Arch package keyring.\n"
+	pacman -Sy --noconfirm archlinux-keyring
+
+	printf "Installing base Arch packages.\n"
+	pacstrap /mnt base base-devel
+
+	printf "Creating fstab with root/swap/UEFI.\n"
+	genfstab -U /mnt >> /mnt/etc/fstab
+	
+	printf "Chrooting into installation.\n"
+	curl https://raw.githubusercontent.com/georgeabr/arch/master/arch-2.sh > arch-2.sh; chmod +x arch-2.sh; cp ./arch-2.sh /mnt; arch-chroot /mnt /bin/bash -c "./arch-2.sh"
+	# arch-chroot /mnt
+
+	
+# #en_GB.UTF-8 UTF-8
+# grep -rl "#en_GB.UTF-8 UTF-8" /etc/locale.gen | xargs sed -i 's/#en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/g'
+}
+
+leave_now()
+{
+	printf "Will leave now!!\n";
+}
+
+# test
+
+echo ""
+read -r -p "Are you sure? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        go_ahead
+        ;;
+    *)
+        leave_now
+        ;;
+esac

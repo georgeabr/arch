eb057d6bc6a8f012b2e6a736b2581d9780ab407c
George Abraham <37941352+georgeabr@users.noreply.github.com>
2019-12-26 19:08:11 +0000

    Update arch.sh

--- VM-xfce/arch.sh
+++ VM-xfce/arch.sh
@@ -50,8 +50,10 @@ fi
 go_ahead()
 {
 	printf "Using UK mirrors\n"
-	pacman_file="/etc/pacman.d/mirrorlist"; 
-	printf "Server = http://archlinux.uk.mirror.allworldit.com/archlinux/\$repo/os/\$arch\n" > $pacman_file;
+	pacman -Sy --noconfirm pacman-contrib
+	curl -s "https://www.archlinux.org/mirrorlist/?&country=GB&country=FR&country=NL&protocol=http&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist 
+	# pacman_file="/etc/pacman.d/mirrorlist"; 
+	# printf "Server = http://archlinux.uk.mirror.allworldit.com/archlinux/\$repo/os/\$arch\n" > $pacman_file;
 	# printf "Server = http://mirror.bytemark.co.uk/archlinux/\$repo/os/\$arch\n" >> $pacman_file;
 	
 	printf "\nPart 1 - Initial disk formatting/bootstrap/installation.\n";
